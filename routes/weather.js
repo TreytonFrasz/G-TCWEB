2ca2933f3c2abaf7a57d40acd5230c95d2dfc817
TreytonFrasz <Treyton88@gmail.com>
2024-10-07 13:21:02 -0600

    Hopefully this works this time

---
+++ routes/weather.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('Weather', { title: 'Weather Search' });
+});
+
+// document.getElementById('weatherForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     const location = document.getElementById('locationInput').value;
+//     const apiKey = ''; // Replace with API key
+//     const url = ``;
+//
+// });
+
+module.exports = router;
